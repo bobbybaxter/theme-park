a9a863ce6f8e8fcf7b6c97cb9747741238e03838
Bob <bob.e.baxter@gmal.com>
2019-04-04 18:03:31 -0500

    wip

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-console.log('theme park');
+// XHR - XML HTTP Request
+let rides = [];
+
+const domStringBuilder = (arrayToPrint) => {
+  console.log('arrayToPrint', arrayToPrint);
+  // loop over arrayToPrint and build up domString
+  // call printToDom
+};
+
+// can't do a fat arrow function because of "this"
+function executeThisCodeAfterFileLoads(){
+  const data = JSON.parse(this.responseText);
+  rides = data.rides;
+  domStringBuilder(data.rides);
+};
+
+function executeThisCodeIfXHRFails(){
+  console.error('doh!');
+}
+
+const getRidesData = () => {
+  const myRequest = new XMLHttpRequest();
+  myRequest.addEventListener('load', executeThisCodeAfterFileLoads);
+  myRequest.addEventListener('error', executeThisCodeIfXHRFails);
+  myRequest.open('GET', './db/rides.json');
+  console.log(myRequest);
+  myRequest.send();
+};
+
+const init = () => {
+  getRidesData();
+};
+
+init();
